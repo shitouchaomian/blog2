42fcb7374aa7919586c3e44337df22055d5e8b88
shitouchaomian <903565442@qq.com>
2019-10-27 15:10:59 +0800

    xxx

--- article/urls.py
+++ article/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'article'
 
 urlpatterns = [
-
     path('article-list/', views.article_list, name='article_list'),
     path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
+    path('article-create/', views.article_create, name='article_create'),
     # 目前还没有urls
 ]
