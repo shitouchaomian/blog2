e452f6fa9527bdccc94286062cb78ef8fcf30b21
shitouchaomian <903565442@qq.com>
2019-10-30 17:59:48 +0800

    xxx

--- article/urls.py
+++ article/urls.py
@@ -11,9 +11,7 @@ urlpatterns = [
     path('article-create/',views.article_create,name='article_create'),
     path('article-delete/<int:id>/',views.article_delete,name='article_delete'),
     #  安全删除文章
-    path(
-        'article-safe-delete/<int:id>/',
-        views.article_safe_delete,
-        name='article_safe_delete'
-        ),
+    path('article-safe-delete/<int:id>/',views.article_safe_delete,name='article_safe_delete'),
+    #更新文章
+    path('article-update/<int:id>/', views.article_update, name='article_update'),
 ]
